ecdb0f1dd5cb76dff14bc2629e53a2d2e0f14064
Miloky <monmorti@gmail.com>
2024-02-05 01:03:12 +0200

    fix build

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const $root = document.getElementById('root') as HTMLDivElement;
+const $root = document.getElementById("root") as HTMLDivElement;
 const root = ReactDOM.createRoot($root);
 
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
 
